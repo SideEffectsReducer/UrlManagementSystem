3a0f740482e77806c1d3d6bad5ade1b854234445
SideEffectsReducer <Andrzej.Mikulec.pl@gmail.com>
2021-10-13 23:20:34 +0200

    add-url: disabled test for html check

--- src/app/add-url/add-url.component.spec.ts
+++ src/app/add-url/add-url.component.spec.ts
@@ -1,18 +1,26 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture,TestBed} from '@angular/core/testing';
 import {SaveUrlService} from '../save-url.service';
 import {AddUrlComponent} from './add-url.component';
 import {first} from 'rxjs/operators';
+import {UrlModel} from '../shared/models/url.model';
+import { DebugElement } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('AddUrlComponent', () => {
   let component: AddUrlComponent;
   let saveUrlService: SaveUrlService;
+  let fixture: ComponentFixture<AddUrlComponent>;
   class MockSaveUrlService{
     save = jasmine.createSpy('save');
   }
 
-  beforeEach(async () => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
     // provide the component-under-test and dependent service
+    declarations:[AddUrlComponent],
+    imports: [FormsModule, HttpClientModule],
     providers: [
       AddUrlComponent,
       { provide: SaveUrlService, useClass: MockSaveUrlService }
@@ -20,7 +28,9 @@ describe('AddUrlComponent', () => {
   });
 
   // inject both the component and the dependent service.
+
   component = TestBed.inject(AddUrlComponent);
+  fixture = TestBed.createComponent(AddUrlComponent);
   saveUrlService = TestBed.inject(SaveUrlService);
   });
 
@@ -86,4 +96,46 @@ describe('AddUrlComponent', () => {
     // assert
     expect(saveUrlService.save).toHaveBeenCalledWith(component.urlRecord);
   });
+
+  // it('submitted html form should call save-url service with correct data', (done) => {
+  //   // arrange
+  //   const expectedUrlObject: UrlModel =
+  //   {
+  //     'title': 'Mock title',
+  //     'tagName': 'Mock tag',
+  //     'url': 'Mock url',
+  //     'urlLocation': 'Mock url location',
+  //     'active': true,
+  //     'type': 'Mock type',
+  //     'pdfLocation': 'Mock pdf location',
+  //     'pdfStored': true,
+  //     'urlTracked': true,
+  //   };
+  //   supplyHtmlTagWithContent('#title', expectedUrlObject.title.toString());
+  //   supplyHtmlTagWithContent('#tagName', expectedUrlObject.tagName.toString());
+  //   supplyHtmlTagWithContent('#url', expectedUrlObject.url.toString());
+  //   supplyHtmlTagWithContent('#urlLocation', expectedUrlObject.urlLocation.toString());
+  //   supplyHtmlTagWithContent('#pdfStored', expectedUrlObject.pdfStored.toString());
+  //   supplyHtmlTagWithContent('#urlTracked', expectedUrlObject.urlTracked.toString());
+  //   const root = fixture.debugElement;
+  //   let addButton: DebugElement = root.query(By.css('#addButton'));
+
+  //   // act
+  //   addButton.triggerEventHandler("click", null);
+  //   setTimeout(() => checkResult(), 1000);
+
+  //   // assert
+  //   fixture.detectChanges();
+  //   function checkResult(){
+  //   expect(saveUrlService.save).toHaveBeenCalled();
+  //   done();
+  //   }
+
+  //   function supplyHtmlTagWithContent(tagId: string, content: string){
+  //     const root = fixture.debugElement.nativeElement;
+  //     let element: HTMLElement = root.querySelector(tagId);
+  //     element.textContent = content;
+  //   }
+  // })
+
 });
