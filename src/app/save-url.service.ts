e0bfd83a2dd3220def6df18b520108b1105dfca8
SideEffectsReducer <Andrzej.Mikulec.pl@gmail.com>
2021-10-13 19:35:37 +0200

    save-url: refactoring

--- src/app/save-url.service.ts
+++ src/app/save-url.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UrlModel } from "./shared/models/url.model";
-
 @Injectable({
   providedIn: 'root'
 })
 export class SaveUrlService {
-
   private _url = "http://localhost:3000/api/example/add";
 
   constructor(private _http: HttpClient) { }
 
-  save(urlModel : UrlModel){
-
-    this._http.post<any>(this._url, urlModel).subscribe(
-      data => console.log("success!", data),
-      error => console.log("error!", error)
+  save(urlModel : UrlModel): void{
+    this._http.post<string>(this._url, urlModel).subscribe(
+      () => alert('success!'),
+      (error) => alert('error!\n' + error)
     )
   }
-
 }
