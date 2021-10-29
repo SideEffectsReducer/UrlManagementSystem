10de4d451d9dccae6f66855ad1c54bb0864ca399
SideEffectsReducer <Andrzej.Mikulec.pl@gmail.com>
2021-10-29 15:26:20 +0200

    pdf_processing: once pdf genereation exception thrown sever should continue

--- server/pdf_processing.js
+++ server/pdf_processing.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
-
 async function createPdf(siteUrl, pdfName, locationPath) {
+try{
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(siteUrl, {
@@ -10,6 +10,10 @@ async function createPdf(siteUrl, pdfName, locationPath) {
   await page.pdf({ path: locationPath + "/" + pdfName + '.pdf', format: 'a4' });
 
   await browser.close();
+}catch(err){
+  console.log(err);
+}
+
 }
 
 module.exports = { createPdf };
